17a4a1bda81bd8012aa0374fce353820a504f556
ayushmohanawasthi <ayush.awasthi@lenkart.in>
2023-05-02 19:19:38 +0530

    added category filter

--- distributor.py
+++ distributor.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask,jsonify,request,render_template,redirect,url_for
-from fetch_item_list import fetch_item_list
+from fetch_item_list import fetch_item_list, fetch_catagory
 
 app = Flask(__name__,template_folder='templates')
 
@@ -10,9 +10,19 @@ def home():
 
 @app.route('/create_so', methods = ['POST', 'GET'])
 def create_so():
-    list_user = fetch_item_list()
-    #list_user = ["C++", "Python", "PHP", "Java", "C", "Ruby","R", "C#", "Dart", "Fortran", "Pascal", "Javascript"]
+    list_user = fetch_catagory()
     return render_template('create_sale_order.html', languages=list_user)
+
+@app.route('/category',methods=['POST'])
+def submitcateg():
+    if request.method == 'POST':
+        input_json = request.form.get("category")
+        input_json = input_json.split("~")[0]
+        print(input_json)
+        list_user = fetch_item_list(input_json)
+        print(list_user)
+        return render_template('create_sale_order.html', languages=list_user)
+
 
 
 app.config['DEBUG']= True
