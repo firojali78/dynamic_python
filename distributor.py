d59403826fd6cde09e5e67486163f76384fbdd59
ayushmohanawasthi <ayush.awasthi@lenkart.in>
2023-05-02 17:26:55 +0530

    added autocomplete

--- distributor.py
+++ distributor.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask,jsonify,request,render_template,redirect,url_for
-
+from fetch_item_list import fetch_item_list
 
 app = Flask(__name__,template_folder='templates')
 
@@ -8,6 +8,11 @@ app = Flask(__name__,template_folder='templates')
 def home():
     return render_template('index.html')
 
+@app.route('/create_so', methods = ['POST', 'GET'])
+def create_so():
+    list_user = fetch_item_list()
+    #list_user = ["C++", "Python", "PHP", "Java", "C", "Ruby","R", "C#", "Dart", "Fortran", "Pascal", "Javascript"]
+    return render_template('create_sale_order.html', languages=list_user)
 
 
 app.config['DEBUG']= True
