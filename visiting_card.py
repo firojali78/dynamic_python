054dc3a6e68b77bd2a2099d8fe8ffd04e9885a28
ayushmohanawasthi <ayush.awasthi@lenkart.in>
2023-07-24 18:36:38 +0530

    added visiting card

---
+++ visiting_card.py
@@ -0,0 +1,30 @@
+import requests
+import json
+from requests_ntlm import HttpNtlmAuth
+from flask import Flask, jsonify, request, render_template, redirect, url_for
+import logging
+from datetime import date, datetime
+
+
+logging.basicConfig(filename="/Users/ayushmohanawasthi/IdeaProjects/Automation/automation/scm-automation-python/logs/" + str(date.today()) + ".log", format='%(asctime)s %(message)s', filemode='a')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# setting up flask app
+app = Flask(__name__, template_folder='template')
+
+
+
+@app.route('/visiting/<name>/<designation>/<company_name>/<email>/<phone>/<website>', methods=['GET'])
+def visiting_card_details(name,designation,company_name,email,phone,website):
+    print(name,designation,company_name,email,phone,website)
+    return render_template("index.html", name= name, designation=designation,company_name=company_name,email=email,phone=phone,website=website)
+
+
+
+app.config["DEBUG"] = True
+
+# starting flask application
+if __name__ == '__main__':
+    # app.run(debug=True, port=6262)
+    app.run(host='127.0.0.1', port=6262, debug=True)
--- whatsapp_bot.py
+++ whatsapp_bot.py
@@ -7,4 +7,4 @@ import pywhatkit
 #pywhatkit.sendwhatmsg_to_group("write-id-here", "Message 3", 19, 2)
 
 pywhatkit.sendwhats_image("+919891456287","/Users/ayushmohanawasthi/Downloads/bodyCare.jpg", "Firoj Sir is great!", 11, True, 2)
-pywhatkit.sendwhatmsg_instantly("+919891456287","Hi", 5, True, 2)
+#pywhatkit.sendwhatmsg_instantly("+919891456287","Hi", 5, True, 2)
